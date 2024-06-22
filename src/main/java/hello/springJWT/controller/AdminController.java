45a044a1e846b2da257f33f4127b662a67b84ede
iju42829 <ijud4282@gmail.com>
2024-06-22 14:15:52 +0900

    feat : 기본 controller 생성

---
+++ src/main/java/hello/springJWT/controller/AdminController.java
@@ -0,0 +1,15 @@
+package hello.springJWT.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminHome() {
+        return "admin home page";
+    }
+}
